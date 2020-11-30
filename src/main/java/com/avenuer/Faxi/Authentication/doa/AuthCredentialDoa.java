1f6d657fe081275a34f6fd83362bf4bd890e03ce
Oyegoke Abiodun Ogundijo <abiodunogundijo@gmail.com>
2020-11-30 08:25:23 +0100

    Created the Entity model for AuthCredential

--- src/main/java/com/avenuer/Faxi/Authentication/doa/AuthCredentialDoa.java
+++ src/main/java/com/avenuer/Faxi/Authentication/doa/AuthCredentialDoa.java
@@ -1,4 +1,14 @@
 package com.avenuer.Faxi.Authentication.doa;
 
-public class AuthCredentialDoa {
+import com.avenuer.Faxi.Authentication.models.AuthCredential;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuthCredentialDoa extends CrudRepository<AuthCredential, String> {
+
+    Optional<AuthCredential> findByEmail(String email);
 }
--- src/main/java/com/avenuer/Faxi/Authentication/models/AuthCredential.java
+++ src/main/java/com/avenuer/Faxi/Authentication/models/AuthCredential.java
@@ -1,13 +1,25 @@
 package com.avenuer.Faxi.Authentication.models;
 
+import com.avenuer.Faxi.Authentication.enums.ROLE.ROLE;
 import lombok.Builder;
+import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
 
 @Builder
-@Entity
+@Data
+@Entity(name = "auth_credentials")
 public class AuthCredential {
+    @Id
+    @GeneratedValue
+    private UUID Id;
     private String email;
     private String passwordHash;
-    private String Id;
+    private ROLE role;
+
+    private String modifiedBy;
+    private Date dateCreated;
+    private Date dateModified;
 }
