bc1749129fd66999712dc72652167653d9b41641
Oyegoke Abiodun Ogundijo <abiodunogundijo@gmail.com>
2021-01-17 00:31:14 +0100

    Wallet Package Scalfolding
    Created wallet and wallet transaction models and basic utitlity functions via ORM in preparation for creating transactional procedures.

---
+++ src/main/java/com/avenuer/faxi/wallets/models/WalletTransaction.java
@@ -0,0 +1,42 @@
+package com.avenuer.faxi.wallets.models;
+
+import com.avenuer.faxi.wallets.enums.TransactionType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity(name = "wallet_transactions")
+public class WalletTransaction {
+
+    @JsonIgnore
+    @Id
+    private UUID id;
+
+    private double amount;
+
+    @Column(nullable = false)
+    private TransactionType transactionType;
+
+    @Column(nullable = false)
+    private String reference;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @CreatedDate
+    private Date dateCreated;
+
+    @LastModifiedDate
+    private Date dateModified;
+
+}
