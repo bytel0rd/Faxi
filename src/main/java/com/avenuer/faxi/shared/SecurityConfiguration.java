799eea90038c6aa4872554f0c8777e7be6edf1ea
Oyegoke Abiodun Ogundijo <abiodunogundijo@gmail.com>
2021-02-06 12:46:32 +0100

    Partial Implementation of Error handler formatter

--- src/main/java/com/avenuer/faxi/shared/SecurityConfiguration.java
+++ src/main/java/com/avenuer/faxi/shared/SecurityConfiguration.java
@@ -1,8 +1,11 @@
-package com.avenuer.faxi.authentication.config;
+package com.avenuer.faxi.shared;
 
+import com.avenuer.faxi.authentication.config.FaxiUserDetailService;
 import com.avenuer.faxi.authentication.services.PasswordService;
+import com.avenuer.faxi.shared.configs.ResponseErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
         securedEnabled = true,
@@ -26,6 +32,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordService passwordService;
 
+    @Autowired
+    private ResponseErrorHandler errorResponseHandeler;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService);
@@ -35,6 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http = http.cors().and().csrf().disable();
+        http = http.addFilterBefore(errorResponseHandeler, CorsFilter.class);
 
         http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and();
---
+++ src/main/java/com/avenuer/faxi/users/Services/TraditionalBankingInformationService.java
@@ -0,0 +1,55 @@
+package com.avenuer.faxi.users.Services;
+
+import com.avenuer.faxi.authentication.models.AuthProfile;
+import com.avenuer.faxi.users.Models.TraditionalBankingInformation;
+import com.avenuer.faxi.users.Params.BVNUpdateParam;
+import com.avenuer.faxi.users.Params.TraditionalAccountUpdateParam;
+import com.avenuer.faxi.users.doa.TraditionalBankingInformationDoa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Service
+public class TraditionalBankingInformationService {
+
+    @Autowired
+    private TraditionalBankingInformationDoa traditionalBankingDoa;
+
+    public TraditionalBankingInformation updateBvnInformation(AuthProfile currentUser, BVNUpdateParam bvnInformation) {
+        TraditionalBankingInformation accountDetailsToUpdate = traditionalBankingDoa.findByUserId(bvnInformation.getUserId());
+
+        if (accountDetailsToUpdate == null) {
+            accountDetailsToUpdate = TraditionalBankingInformation.builder()
+                    .userId(bvnInformation.getUserId())
+                    .build();
+        }
+
+        if (false) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "UnMatch Data provided during BVN verification");
+        }
+
+        accountDetailsToUpdate.setBVN(bvnInformation.getBVN());
+        accountDetailsToUpdate.setModifiedBy(currentUser.fullName());
+
+        return traditionalBankingDoa.save(accountDetailsToUpdate);
+    }
+
+    TraditionalBankingInformation updateBankInformation(AuthProfile currentUser, TraditionalAccountUpdateParam accountInformation) {
+        TraditionalBankingInformation accountDetailsToUpdate = traditionalBankingDoa.findByUserId(accountInformation.getUserId());
+
+        if (accountDetailsToUpdate == null) {
+            accountDetailsToUpdate = TraditionalBankingInformation.builder()
+                    .userId(accountInformation.getUserId())
+                    .build();
+        }
+
+        accountDetailsToUpdate.setBankCode(accountInformation.getBankCode());
+        accountDetailsToUpdate.setBankAccountNumber(accountInformation.getBankAccountNumber());
+        accountDetailsToUpdate.setModifiedBy(currentUser.fullName());
+
+        return traditionalBankingDoa.save(accountDetailsToUpdate);
+
+    }
+
+}
